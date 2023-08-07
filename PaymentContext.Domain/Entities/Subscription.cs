3b7dbf43637c97ac71855618ffbc83d44fa8c226
Victor Siqueira <siqueira_2004@hotmail.com>
2023-08-06 23:45:00 -0300

    feat: add entities

---
+++ PaymentContext.Domain/Entities/Subscription.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PaymentContext.Domain.Entities
+{
+    public class Subscription
+    {
+        public DateTime CreateDate { get; private set; }
+        public DateTime LastUpdateDate { get; private set; }
+        public DateTime? ExpireDate { get; private set; }
+        public bool Active { get; private set; }
+        public List<Payment> Payments { get; set; }
+    }
+}
